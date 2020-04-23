34b2619b7e92a1ead6307d7b0becf05bcc6daed7
Paul D. Duncan <chesiredarling@gmail.com>
2020-04-23 14:45:24 -0700

    The Initial

---
+++ src/SongPlayer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AddSong() {
+  return (
+    <div>
+      SongPlayer
+      <QueuedSongList />
+    </div>
+  );
+}
+
+export default SongPlayer;
---
+++ src/components/SongList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AddSong() {
+  return <div>SongList</div>;
+}
+
+export default SongList;
