63317c80f45096c7c1284efa0e71a8c8582fa959
Paul D. Duncan <chesiredarling@gmail.com>
2020-04-24 08:50:05 -0700

    add Material-ui to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core';
+import { HeadsetTwoTone } from '@material-ui/icons';
 
-export const Header = () => {
-  return <div></div>;
-};
+const useStyles = makeStyles(theme => ({
+    title: {
+        marginLeft: theme.spacing(2)
+    }
+}))
+function Header() {
+    const slasses = useStyles()
+    return (
+        <AppBar color="secondary" position='fixed'>
+            <Toolbar>
+                <HeadsetTwoTone />
+                <Typography className={classes.title} variant="h6" component="h1">
+                    Apaullo Flow Store
+                </Typography>
+            </Toolbar>
+
+        </AppBar>
+    )
+}
